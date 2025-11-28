b53026078503d68660165ad530fa1a14bd79274f
TonyBryan02 <bryantchouta19@gmail.com>
2025-11-28 17:53:36 -0500

    Creation of JS file and Completion of Variables definitions and random functions
    The work was done in the MDN website link so I'm copying and pasting my work

---
+++ lab4_files/ass4_pt1/ass4_pt1.js
@@ -0,0 +1,10 @@
+// Complete variable definitions and random functions
+
+const customName = document.getElementById('custom-name');
+const generateBtn = document.querySelector('.generate');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
